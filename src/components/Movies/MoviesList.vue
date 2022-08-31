d58694713bb4cfcccb133c65a60345e6b9eb8acf
Dejan Conkic <conkicdejan@gmail.com>
2022-08-31 13:30:27 +0200

    Add Pagination

--- src/components/Movies/MoviesList.vue
+++ src/components/Movies/MoviesList.vue
@@ -1,7 +1,7 @@
 <template>
   <ul>
     <div className="card-group container-fluid mt-2">
-      <div v-for="movie in moviesStore.movies" :key="movie.id">
+      <div v-for="movie in moviesStore.movies.data" :key="movie.id">
         <MovieItem :movie="movie" />
       </div>
     </div>
