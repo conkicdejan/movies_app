81b73877010a029433febf781bb024556a19d2d7
Dejan Conkic <conkicdejan@gmail.com>
2022-09-08 12:19:28 +0200

    Add create-movie-from-OMDB

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import { createPinia } from 'pinia'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { createPinia } from 'pinia';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import vSelect from 'vue-select';
+import 'vue-select/dist/vue-select.css';
 
-const pinia = createPinia()
-createApp(App).use(pinia).use(router).mount('#app')
+const pinia = createPinia();
+createApp(App)
+  .component('v-select', vSelect)
+  .use(pinia)
+  .use(router)
+  .mount('#app');
